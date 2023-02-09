ac8161a57de0bfa49cab56bcafdc93fce4c5182a
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-02-09 09:33:54 +0100

    prettier refactor

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,82 +1,61 @@
 "use strict";
 
-
 const EmptyCartException = require("./EmptyCartException.js");
 const UpdateCartException = require("./UpdateCartException.js");
 module.exports = class Cart {
+  #items;
 
-    #items;
+  constructor(items) {
+    //forced to do so because of total_EmptyCart_ThrowException test
+    this.#items = items;
+  }
 
-    constructor(items) {
-
-        //forced to do so because of total_EmptyCart_ThrowException test
-        this.#items = items;
-
+  get items() {
+    if (this.#items == null) {
+      throw new EmptyCartException();
     }
 
-    get items() {
-        if (this.#items == null) {
+    return this.#items;
+  }
 
-            throw new EmptyCartException();
+  get total() {
+    if (this.#items == null) {
+      throw new EmptyCartException();
+    }
+    return this.calculateTotal(this.#items, false, true);
+  }
 
-        }
-
-        return this.#items;
+  count(isSum) {
+    if (this.#items == null) {
+      throw new EmptyCartException();
     }
 
-    get total() {
-        if (this.#items == null) {
+    return this.calculateTotal(this.#items, isSum, false);
+  }
 
-            throw new EmptyCartException();
+  calculateTotal(items, isSum, isTotal) {
+    let sum = 0;
 
-        }
-        return this.calculateTotal(this.#items, false, true);
+    items.forEach(function (item) {
+      if (isSum) {
+        sum += 1;
+      } else if (isTotal) {
+        sum += item.price * item.quantity;
+      } else {
+        sum += item.quantity;
+      }
+    });
+    return sum;
+  }
+
+  add(item) {
+    if (item == null) {
+      throw new UpdateCartException();
     }
-
-    count(isSum) {
-
-        if (this.#items == null) {
-
-            throw new EmptyCartException();
-
-        }
-
-
-        return this.calculateTotal(this.#items, isSum, false);
+    if (this.#items == null) {
+      this.#items = item;
+    } else {
+      this.#items.push(item);
     }
-
-    calculateTotal(items, isSum, isTotal) {
-        let sum = 0
-
-        items.forEach(function (item) {
-            if (isSum) {
-                sum += 1;
-            } else if (isTotal) {
-                sum += item.price * item.quantity;
-            } else {
-                sum += item.quantity;
-
-            }
-
-        });
-        return sum
-    }
-
-    add(item) {
-
-        if (item == null) {
-
-            throw new UpdateCartException();
-
-        }
-        if (this.#items == null) {
-
-            this.#items = item;
-
-        } else {
-
-            this.#items.push(item);
-        }
-    }
-
-}
+  }
+};
--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -5,71 +5,62 @@ const InvalidQuantityException = require("./InvalidQuantityException.js");
 const InvalidPriceException = require("./InvalidPriceException.js");
 
 module.exports = class CartItem {
+  //region private attributes
+  #articleId;
+  #name;
+  #quantity;
+  #price;
+  //endregion private attributes
 
-    //region private attributes
-    #articleId;
-    #name;
-    #quantity;
-    #price;
-    //endregion private attributes
-
-    //region public methods
-    constructor(articleId, name, quantity, price) {
-        if(articleId < 0){
-
-            throw new InvalidArticleIdException();
-
-        }
-
-        this.#articleId = articleId;
-        this.#name = name;
-
-        this.quantity = quantity;
-        this.price = price;
+  //region public methods
+  constructor(articleId, name, quantity, price) {
+    if (articleId < 0) {
+      throw new InvalidArticleIdException();
     }
 
-    get articleId() {
-        return this.#articleId;
+    this.#articleId = articleId;
+    this.#name = name;
+
+    this.quantity = quantity;
+    this.price = price;
+  }
+
+  get articleId() {
+    return this.#articleId;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get quantity() {
+    return this.#quantity;
+  }
+
+  set quantity(value) {
+    if (value < 0) {
+      throw new InvalidQuantityException();
     }
 
-    get name() {
-        return this.#name;
+    this.#quantity = value;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(value) {
+    if (value < 10) {
+      throw new InvalidPriceException();
     }
+    this.#price = value;
+  }
 
-    get quantity() {
-        return this.#quantity
-    }
+  get total() {
+    return this.#price * this.#quantity;
+  }
+  //endregion public methods
 
-    set quantity(value) {
-
-        if(value < 0){
-
-            throw new InvalidQuantityException();
-
-        }
-
-        this.#quantity = value;
-    }
-
-    get price() {
-        return this.#price
-    }
-
-    set price(value) {
-        if(value < 10){
-            throw new InvalidPriceException();
-        }
-        this.#price = value;
-    }
-
-    get total() {
-        return this.#price * this.#quantity;
-    }
-    //endregion public methods
-
-    //region private methods
-    //endregion private methods
-}
-
-
-
+  //region private methods
+  //endregion private methods
+};
